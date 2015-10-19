074dd6602a5f0b9212ec3bf8b7e12c4721da37ae
shrutig <shruti.999.999@gmail.com>
2015-10-19 13:41:57 +0530

    Confusion matric computation changed

--- aws/src/main/scala/io/ddf/aws/ml/MLSupporter.scala
+++ aws/src/main/scala/io/ddf/aws/ml/MLSupporter.scala
@@ -104,11 +104,11 @@ class MLSupporter(ddf: DDF) extends ADDFFunctionalGroupHandler(ddf) with ISuppor
     for (row <- result.indices) {
       val newVal = result(row)._2.asInstanceOf[Double]
       val oldVal = (List(result(row)._1) collect { case i: java.lang.Number => i.doubleValue() }).sum
-      if (oldVal < v && newVal < v)
+      if ((oldVal < v || oldVal == v) && (newVal < v || newVal == v))
         matrix(0)(0) = matrix(0)(0) + 1
-      else if (oldVal < v && newVal > v)
+      else if ((oldVal < v || oldVal == v) && newVal > v)
         matrix(0)(1) = matrix(0)(1) + 1
-      else if (oldVal > v && newVal < v)
+      else if (oldVal > v && (newVal < v || newVal == v))
         matrix(1)(0) = matrix(1)(0) + 1
       else
         matrix(1)(1) = matrix(1)(1) + 1
